19057c3653050e53706b2adbb4d1606af260ea9e
Konagawa <konashin215@me.com>
2024-06-23 22:57:28 +0900

    create user comp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController # rubocop:disable Layout/EndOfLine,Style/Documentation
+  def show
+    # 　Viewから与えられた引数をもとにUsersテーブルの該当レコードを検索し、変数に格納
+    @user = User.find(params[:id])
+  end
+
   def new
+    #  Userクラスのオブジェクトを作成して、viewに渡す
+    @user = User.new
+  end
+
+  def create
+    #  多分ストロングパラメーター為に書いてるぽい、セキュリティかな
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ログインしました。'
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # メールアドレスをバリデーション前に全て小文字に変換するコールバック
+  before_save { email.downcase! }
+  #presence: trueは空打ち禁止用
+  validates :name, presence: true, length: { maximum: 50 }
+  # メールの正規表現を定義
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
