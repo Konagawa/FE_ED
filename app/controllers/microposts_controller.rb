222190190e05cc4ec2d0463d9b468400394cfc44
Konagawa <konashin215@me.com>
2024-07-06 13:06:56 +0900

    一覧作成

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,32 @@
 class MicropostsController < ApplicationController
-  def create
+
+  def index
+    @posts = Micropost.all
   end
+
+  def new
+    @post = Micropost.new
+  end
+
+  def create
+    @post = current_user.microposts.build(micropost_params)
+    if @post.save
+      redirect_to @post, notice: '投稿が作成されました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Micropost.find_by(params[:id])
+  end
+
   def destroy
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get "/help",     to: "static_pages#help"
-  get "/contact",  to: "static_pages#contact"
-  get "/policy",   to: "static_pages#policy"
-  get "/signup",   to: "users#new"
+  get "/help",       to: "static_pages#help"
+  get "/contact",    to: "static_pages#contact"
+  get "/policy",     to: "static_pages#policy"
+  get "/signup",     to: "users#new"
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
+  get "/post",       to: "microposts#show"
+
   resources :users
   resources :microposts
 end
